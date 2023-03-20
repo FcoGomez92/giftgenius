d23f88a959ecc0e5192f1be1e0dcd6f25cb8c67a
Francisco Manuel Gómez Gallardo <francis.gg48@gmail.com>
2023-03-20 15:38:25 +0100

    integrate openAI sdk

---
+++ src/lib/openai.js
@@ -0,0 +1,21 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const { OPENAI_API_KEY } = process.env
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(configuration)
+
+export async function talkToChatGPT(prompt) {
+  try {
+    return await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: prompt }]
+    })
+  } catch (error) {
+    console.log(error)
+    return error.response.data
+  }
+}
