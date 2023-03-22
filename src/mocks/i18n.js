8a029e5c6946069d829df930e9185953d7937047
Francisco Manuel Gómez Gallardo <francis.gg48@gmail.com>
2023-03-22 17:18:32 +0100

    add a first i18n homemade attempt

---
+++ src/mocks/i18n.js
@@ -0,0 +1,10 @@
+export const i18n = {
+  prefix: {
+    es: 'Estudia los gustos, necesidades y personalidad de la persona que ha escrito los siguientes twits. Dame una lista de 5 productos que pueda comprar en Amazon para regalarle en su cumpleaños:\n\n',
+    en: 'Study the personal tastes, needs, and personality of the person who has written the following tweets. Give me a list of 5 products that I can buy on Amazon as a gift for their birthday:\n\n'
+  },
+  suffix: {
+    es: '\n\nEvita dar respuestas genéricas.',
+    en: '\n\nAvoid giving generic answers.'
+  }
+}
